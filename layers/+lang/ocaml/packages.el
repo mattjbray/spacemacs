1cb8f9d787e93fb1aa686fcac2563aa69e7bd864
Matt Bray <mattjbray@gmail.com>
2018-01-29 21:16:50 +0000

    ocaml: Fix loading flycheck-ocaml
    Before this change, we get the warning:
    
        - package flycheck-ocaml not initialized in layer ocaml, you may consider removing
          this package from the package list or use the :toggle keyword instead of a
          `when' form.
    
    And spacemacs does not install flycheck-ocaml.

--- layers/+lang/ocaml/packages.el
+++ layers/+lang/ocaml/packages.el
@@ -13,8 +13,8 @@
   '(
     ;; auto-complete
     company
-    flycheck
-    flycheck-ocaml
+    (flycheck :when (configuration-layer/layer-used-p 'syntax-checking))
+    (flycheck-ocaml :when (configuration-layer/layer-used-p 'syntax-checking))
     ggtags
     counsel-gtags
     helm-gtags
@@ -32,18 +32,19 @@
       :modes merlin-mode
       :variables merlin-completion-with-doc t)))
 
-(when (configuration-layer/layer-used-p 'syntax-checking)
-  (defun ocaml/post-init-flycheck ()
-    (spacemacs/enable-flycheck 'tuareg-mode))
-  (defun ocaml/init-flycheck-ocaml ()
-    (use-package flycheck-ocaml
-      :if (configuration-layer/package-used-p 'flycheck)
-      :defer t
-      :init
-      (progn
-        (with-eval-after-load 'merlin
-          (setq merlin-error-after-save nil)
-          (flycheck-ocaml-setup))))))
+(defun ocaml/post-init-flycheck ()
+  (when (configuration-layer/package-used-p 'flycheck-ocaml)
+    (spacemacs/enable-flycheck 'tuareg-mode)))
+
+(defun ocaml/init-flycheck-ocaml ()
+  (use-package flycheck-ocaml
+    :if (configuration-layer/package-used-p 'flycheck)
+    :defer t
+    :init
+    (progn
+      (with-eval-after-load 'merlin
+        (setq merlin-error-after-save nil)
+        (flycheck-ocaml-setup)))))
 
 (defun ocaml/post-init-ggtags ()
   (add-hook 'ocaml-mode-local-vars-hook #'spacemacs/ggtags-mode-enable))
